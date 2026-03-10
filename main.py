7c87c7967a03aeee95f42f046d149874ea4e5121
Noah <enyuispoop@gmail.com>
2026-03-10 06:38:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import date
 st.set_page_config(page_title="Petrol Pump Cash Counter", layout="wide")
 st.title("⛽ Petrol Pump Cash Counter")
 st.markdown(
-    "<p style='text-align:right;'>(created by nazeeh)</p>",
+    "<p style='text-align:right;'>(Created By Nazeeh)</p>",
     unsafe_allow_html=True
 )
 
@@ -269,6 +269,7 @@ else:
 
         st.session_state.login = False
         st.rerun()
+
 
 
 
